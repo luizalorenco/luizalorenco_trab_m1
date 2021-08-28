a69af151d4325a741fdbbd70fe8f70b4096a6237
luizalorenco <luiza.lorenco@aluno.unc.br>
2021-08-27 23:54:56 -0300

    routes

--- luizalorenco_trab_m1/routes/web.php
+++ luizalorenco_trab_m1/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+    
 Route::get('/', function () {
-    return view('welcome','ProdutoController');
+    return view('welcome');
 });
+
+Route::resource('produtos','ProdutoController');
